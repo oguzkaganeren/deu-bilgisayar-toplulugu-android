888c7acc0d4f4686e51515c14516276bd993724d
Oğuz Kağan EREN <oguzkaganeren@gmail.com>
2017-06-21 01:16:58 +0300

    içerik kısmına cardview eklendi

--- app/src/main/java/com/example/oguz/topluluk/MeetingFragment.java
+++ app/src/main/java/com/example/oguz/topluluk/MeetingFragment.java
@@ -26,7 +26,7 @@ public class MeetingFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_content, container, false);
+        return inflater.inflate(R.layout.fragment_meeting, container, false);
     }
 
 }
